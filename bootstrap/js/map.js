ef791863619dd54f4fa01c2abdd560731318b96a
VENTURA Simone <simone.ventura@soprasteria.com>
2018-10-02 14:29:52 +0200

    First commit

--- bootstrap/js/map.js
+++ bootstrap/js/map.js
@@ -1,4 +1,4 @@
-jsonObj = $.getJSON("https://api.myjson.com/bins/1ggvps",(jsonFile)=>{
+jsonObj = $.getJSON("https://api.myjson.com/bins/61hyo",(jsonFile)=>{
         jsonObj = jsonFile;
 });
 $('document').ready(()=>{
@@ -10,29 +10,36 @@ $('document').ready(()=>{
                 div:'#map',
                 lat:'42.504154',
                 lng:'12.646361',
-                zoom: 5
+                zoom: 5,
+                width: '100%',
+                height:'50%'
+                
         });
         if(jsonObj.readyState != 4){
                 setTimeout(()=>{
                         for(var i=0;i<3;i++){
                                 element = jsonObj[i];
-                                $listbox.html($listbox.html()+"</br><a href='#' id='"+element.Name+"'>"+element.title+"</a>");
+                                $listbox.html($listbox.html()+"</br><button type='button' id='"+element.Name+"' class='btn btn-default'>"+element.Name+"</button>");
+                                //$listbox.html($listbox.html()+"</br><a href='#' id='"+element.Name+"'>"+element.title+"</a>");
                         }
 
-                        $("#Assago").on('click',()=>{
+                        $("#Assago").on('focusin',()=>{
                                 map.removeMarkers();
                                 map.removeOverlays();
                                 marker = map.addMarker(jsonObj[0]);   
                                 map.drawOverlay({
                                         lat: jsonObj[0].lat,
                                         lng: jsonObj[0].lng,
-                                        content: div,
+                                        content: "<div class='overlay'>"+jsonObj[0].description+"</div>",
                                         verticalAlign: 'bottom'
                                 });
+                        }).on('focusout',()=>{
+                                map.removeMarkers();
+                                map.removeOverlays();
                         });
                 
                 
-                        $("#Parma").on('click',()=>{
+                        $("#Parma").on('focusin',()=>{
                                 map.removeMarkers();
                                 map.removeOverlays();
                                 marker = map.addMarker(jsonObj[1]);
@@ -40,12 +47,15 @@ $('document').ready(()=>{
                                 map.drawOverlay({
                                         lat: jsonObj[1].lat,
                                         lng: jsonObj[1].lng,
-                                        content: div,
+                                        content: "<div class='overlay'>"+jsonObj[1].description+"</div>",
                                         verticalAlign: 'bottom'
                                 });
-                        })
+                        }).on('focusout',()=>{
+                                map.removeMarkers();
+                                map.removeOverlays();
+                        });
                 
-                        $('#Padova').on('click',()=>{
+                        $('#Padova').on('focusin',()=>{
                                 map.removeMarkers();
                                 map.removeOverlays();
                                 marker = map.addMarker(jsonObj[2]);
@@ -53,10 +63,13 @@ $('document').ready(()=>{
                                 map.drawOverlay({
                                         lat: jsonObj[2].lat,
                                         lng: jsonObj[2].lng,
-                                        content: div,
+                                        content: "<div class='overlay'>"+jsonObj[2].description+"</div>",
                                         verticalAlign: 'bottom'
                                 });
-                        })
+                        }).on('focusout',()=>{
+                                map.removeMarkers();
+                                map.removeOverlays();
+                        });
                 
 
                 },1000);
