313b18672aba45dc7f7d6d09b289261cb64702e8
VENTURA Simone <simone.ventura@soprasteria.com>
2018-10-02 10:49:42 +0200

    New Commit

--- bootstrap/js/map.js
+++ bootstrap/js/map.js
@@ -1,73 +1,158 @@
+jsonObj = $.getJSON("https://api.myjson.com/bins/1ggvps",(jsonFile)=>{
+        jsonObj = jsonFile;
+});
 $('document').ready(()=>{
 
-        $infobox = $('.col-md-6.info-box');
-
+        const $infobox = $('.col-md-6.info-box');
+        const $listbox = $(".col-md-6.list-box");
+        const div = "<div class='overlay'></div>";
         let map = new GMaps({
                 div:'#map',
                 lat:'42.504154',
                 lng:'12.646361',
                 zoom: 5
         });
+        if(jsonObj.readyState != 4){
+                setTimeout(()=>{
+                        for(var i=0;i<3;i++){
+                                element = jsonObj[i];
+                                $listbox.html($listbox.html()+"</br><a href='#' id='"+element.Name+"'>"+element.title+"</a>");
+                        }
 
-        $("#assago").on('mouseenter',()=>{
-                marker = map.addMarker({
-                        lat:'45.3985562',
-                        lng:'9.1458339',
-                        title:'Sede Assago'
-                });      
-                $infobox.text("Varie info sulla sede di Assago");          
-        }).on('mouseleave',()=>{
-                map.removeMarker(marker);
-                $infobox.text("");
+                        $("#Assago").on('click',()=>{
+                                map.removeMarkers();
+                                map.removeOverlays();
+                                marker = map.addMarker(jsonObj[0]);   
+                                map.drawOverlay({
+                                        lat: jsonObj[0].lat,
+                                        lng: jsonObj[0].lng,
+                                        content: div,
+                                        verticalAlign: 'bottom'
+                                });
+                        });
+                
+                
+                        $("#Parma").on('click',()=>{
+                                map.removeMarkers();
+                                map.removeOverlays();
+                                marker = map.addMarker(jsonObj[1]);
+                                
+                                map.drawOverlay({
+                                        lat: jsonObj[1].lat,
+                                        lng: jsonObj[1].lng,
+                                        content: div,
+                                        verticalAlign: 'bottom'
+                                });
+                        })
+                
+                        $('#Padova').on('click',()=>{
+                                map.removeMarkers();
+                                map.removeOverlays();
+                                marker = map.addMarker(jsonObj[2]);
+                                
+                                map.drawOverlay({
+                                        lat: jsonObj[2].lat,
+                                        lng: jsonObj[2].lng,
+                                        content: div,
+                                        verticalAlign: 'bottom'
+                                });
+                        })
+                
+                        /*$('#Roma').on('click',()=>{
+                                map.removeMarkers();
+                                map.removeOverlays();
+                                marker = map.addMarker(jsonObj[3]);
+                                
+                                map.drawOverlay({
+                                        lat: jsonObj[3].lan,
+                                        lng: jsonObj[3].lng,
+                                        content: div,
+                                        verticalAlign: 'bottom'
+                                });
+                        })
+                
+                        $('#Asti').on('click',()=>{
+                                map.removeMarkers();
+                                map.removeOverlays();
+                                marker = map.addMarker(jsonObj[4]);
+                                
+                                console.log(jsonObj[4].lan);
+                                map.drawOverlay({
+                                        lat: jsonObj[4].lan,
+                                        lng: jsonObj[4].lng,
+                                        content: div,
+                                        verticalAlign: 'bottom'
+                                });
+                        });*/
+
+                },1000);
+        }
+
+         
+        /*$("#Assago").on('click',()=>{
+                map.removeMarkers();
+                map.removeOverlays();
+                marker = map.addMarker(jsonObj[0]);  
+                console.log(jsonObj[0].lat);  
+                map.drawOverlay({
+                        lat: jsonObj[0].lat,
+                        lng: jsonObj[0].lng,
+                        content: $div,
+                        verticalAlign: 'bottom'
+                });
         });
 
 
-        $("#parma").on('mouseenter',()=>{
-                marker = map.addMarker({
-                        lat:'44.7486823',
-                        lng:'10.2009757',
-                        title:'Sede Parma'              
+        $("#Parma").on('click',()=>{
+                map.removeMarkers();
+                map.removeOverlays();
+                marker = map.addMarker(jsonObj[1]);
+                
+                map.drawOverlay({
+                        lat: jsonObj[1].lat,
+                        lng: jsonObj[1].lng,
+                        content: $div,
+                        verticalAlign: 'bottom'
                 });
-                $infobox.text("Varie info sulla sede di Parma");
-        }).on('mouseleave',()=>{
-                map.removeMarker(marker);
-                $infobox.text("");
-        });;
+        })
 
-        $('#padova').on('mouseenter',()=>{
-                marker = map.addMarker({
-                        lat:'45.4086681',
-                        lng:'11.9119778',
-                        title:'Sede Padova'              
+        $('#Padova').on('click',()=>{
+                map.removeMarkers();
+                map.removeOverlays();
+                marker = map.addMarker(jsonObj[2]);
+                
+                map.drawOverlay({
+                        lat: jsonObj[2].lat,
+                        lng: jsonObj[2].lng,
+                        content: div,
+                        verticalAlign: 'bottom'
                 });
-                $infobox.text("Varie info sulla sede di Padova");
-        }).on('mouseleave',()=>{
-                map.removeMarker(marker);
-                $infobox.text("");
-        });;
+        })
 
-        $('#roma').on('mouseenter',()=>{
-                marker = map.addMarker({
-                        lat:'41.83542',
-                        lng:'12.4985713',
-                        title:'Sede Roma'          
+        $('#Roma').on('click',()=>{
+                map.removeMarkers();
+                map.removeOverlays();
+                marker = map.addMarker(jsonObj[3]);
+                
+                map.drawOverlay({
+                        lat: jsonObj[3].lan,
+                        lng: jsonObj[3].lng,
+                        content: div,
+                        verticalAlign: 'bottom'
                 });
-                $infobox.text("Varie info sulla sede di Roma");
-        }).on('mouseleave',()=>{
-                map.removeMarker(marker);
-                $infobox.text("");
-        });;
+        })
 
-        $('#asti').on('mouseenter',()=>{
-                marker = map.addMarker({
-                        lat:'44.8894267',
-                        lng:'8.2093538',
-                        title:'Sede Asti'           
+        $('#Asti').on('click',()=>{
+                map.removeMarkers();
+                map.removeOverlays();
+                marker = map.addMarker(jsonObj[4]);
+                
+                map.drawOverlay({
+                        lat: jsonObj[4].lan,
+                        lng: jsonObj[4].lng,
+                        content: div,
+                        verticalAlign: 'bottom'
                 });
-                $infobox.text("Varie info sulla sede di Asti");
-        }).on('mouseleave',()=>{
-                map.removeMarker(marker);
-                $infobox.text("");
-        });;
+        });*/
         
 });
