4f9253bd182fe8bc9362ca9fb8211c6a977d824c
VENTURA Simone <simone.ventura@soprasteria.com>
2018-10-04 10:35:28 +0200

    Data binding in HTML elements

--- map.js
+++ map.js
@@ -9,9 +9,9 @@ var RestModule = function () {
             callRestService: callRestService
         }
 }();
-
+ 
 var mapHandler = function (){
-
+ 
         function createMap(container,latitude,longitude,zoomIn,width,height){
                 map = new GMaps({
                         div:container,
@@ -23,79 +23,44 @@ var mapHandler = function (){
                 });
                 return map;
         };
-
+ 
         function deleteMap(map){
                 map = null;
         }
-
+ 
         return{
                 createMap : createMap,
                 deleteMap : deleteMap  
         }
 }();
-
-        const $infobox = $('.col-md-6.info-box');
-        const $listbox = $(".col-md-6.list-box");
-        const div = "<div class='overlay'></div>";
-
-        RestModule.callRestService(function(jsonObj){
-                map = mapHandler.createMap("#map","42.7178439","11.192269",5,"100%","50%");
-                for(var i=0;i<3;i++){
-                        element = jsonObj[i];
-                        $listbox.html($listbox.html()+"</br><button type='button' id='"+element.Name+"' class='btn btn-default'>"+element.Name+"</button>");
-                }
+ 
+const $listbox = $(".col-md-6.list-box");
+ 
+RestModule.callRestService(function(jsonObj){
+        map = mapHandler.createMap("#map","42.7178439","11.192269",5,"100%","50%");
+        for(var i=0;i<3;i++){
+                element = jsonObj[i];
+                $listbox.append("</br><button type='button' id='"+element.Name+"' class='btn btn-default btn-gmaps'>"+element.Name+"</button>");
                 
+                $("#" + element.Name).data("name", element.Name);
+                $("#" + element.Name).data("lat", element.lat);
+                $("#" + element.Name).data("lng", element.lng);
+                $("#" + element.Name).data("description", element.description);
+        } 
 
-                $("#Assago").on('focusin',()=>{
-                        map.removeMarkers();
-                        map.removeOverlays();
-                        marker = map.addMarker(jsonObj[0]);   
-                        map.drawOverlay({
-                                lat: jsonObj[0].lat,
-                                lng: jsonObj[0].lng,
-                                content: "<div class='overlay'>"+jsonObj[0].description+"</div>",
-                                verticalAlign: 'bottom'
-                        });
-                }).on('focusout',()=>{
-                        map.removeMarkers();
-                        map.removeOverlays();
+        $(".btn").on('click',function(){
+                //map.removeMarkers();
+                map.removeOverlays();  
+                // TODO - da verificare gestione marker
+                // map.addMarker(element); 
+                map.drawOverlay({
+                        lat: $(this).data("lat"),
+                        lng: $(this).data("lng"),
+                        content: "<div class='overlay'>"+$(this).data("description")+"</div>",
+                        verticalAlign: 'bottom'
                 });
-                        
-                        
-                $("#Parma").on('focusin',()=>{
-                        map.removeMarkers();
-                        map.removeOverlays();
-                        marker = map.addMarker(jsonObj[1]);
-                                        
-                        map.drawOverlay({
-                                lat: jsonObj[1].lat,
-                                lng: jsonObj[1].lng,
-                                content: "<div class='overlay'>"+jsonObj[1].description+"</div>",
-                                verticalAlign: 'bottom'
-                        });
-                }).on('focusout',()=>{
-                        map.removeMarkers();
-                        map.removeOverlays();
-                });
-                        
-                $('#Padova').on('focusin',()=>{
-                        map.removeMarkers();
-                        map.removeOverlays();
-                        marker = map.addMarker(jsonObj[2]);
-                                        
-                        map.drawOverlay({
-                                lat: jsonObj[2].lat,
-                                lng: jsonObj[2].lng,
-                                content: "<div class='overlay'>"+jsonObj[2].description+"</div>",
-                                verticalAlign: 'bottom'
-                        });
-                }).on('focusout',()=>{
-                        map.removeMarkers();
-                        map.removeOverlays();
-                });
-        });
-
-
-
-
-        
+        }).on('focusout',()=>{
+                //map.removeMarkers();
+                map.removeOverlays();
+        });         
+});       
