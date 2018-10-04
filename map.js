a958715d01c7e853ef6b2d5d2a295d261a3da878
VENTURA Simone <simone.ventura@soprasteria.com>
2018-10-04 14:45:22 +0200

    Dynamically and Modular version

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+//Functions Definition
 var RestModule = function () {
         var jsonURL = "https://api.myjson.com/bins/61hyo";
         
@@ -28,42 +29,56 @@ var MapHandler = function (){
                 createMap : createMap
         }
 }();
- //Const declaration
+
+var ButtonHandler = function(){
+        function listBtn(array){
+                for(var i=0;i<array.length;i++){
+                        element = array[i];
+                        //$listbox.append("</br><button type='button' id='"+element.Name+"' class='btn btn-default btn-gmaps'>"+element.Name+"</button>");
+                        $hiddenBtn.clone().attr("id",element.Name).appendTo($listbox).show().text(element.Name);
+                        $("#" + element.Name).data("name", element.Name);
+                        $("#" + element.Name).data("lat", element.lat);
+                        $("#" + element.Name).data("lng", element.lng);
+                        $("#" + element.Name).data("description", element.description);
+                } 
+        
+                $(".btn").on('click',function(){
+                        map.removeMarkers();
+                        map.removeOverlays();  
+                        map.addMarker({
+                                lat: $(this).data("lat"),
+                                lng: $(this).data("lng")    
+                        }); 
+                        map.drawOverlay({
+                                lat: $(this).data("lat"),
+                                lng: $(this).data("lng"),
+                                content: "<div class='overlay'>"+$(this).data("description")+"</div>",
+                                verticalAlign: 'bottom'
+                        });
+                }).on('focusout',()=>{
+                        map.removeMarkers();
+                        map.removeOverlays();
+                });
+        }
+
+        return{
+                listBtn : listBtn
+        }
+}();
+//End of Functions Definition
+
+//Main 
+//Const declaration
 const $listbox = $(".col-md-6.list-box")
         divId = "#map",
         lat = "42.7178439",
         lng = "11.192269",
         zoom = 5,
         width = "100%",
-        height = "50%";
+        height = "50%",
+        $hiddenBtn = $(".btn.btn-default.btn-gmaps");
 
 RestModule.callRestService(function(jsonObj){
         map = MapHandler.createMap(divId,lat,lng,zoom,width,height);
-        for(var i=0;i<jsonObj.length;i++){
-                element = jsonObj[i];
-                $listbox.append("</br><button type='button' id='"+element.Name+"' class='btn btn-default btn-gmaps'>"+element.Name+"</button>");
-                
-                $("#" + element.Name).data("name", element.Name);
-                $("#" + element.Name).data("lat", element.lat);
-                $("#" + element.Name).data("lng", element.lng);
-                $("#" + element.Name).data("description", element.description);
-        } 
-
-        $(".btn").on('click',function(){
-                map.removeMarkers();
-                map.removeOverlays();  
-                map.addMarker({
-                        lat: $(this).data("lat"),
-                        lng: $(this).data("lng")    
-                }); 
-                map.drawOverlay({
-                        lat: $(this).data("lat"),
-                        lng: $(this).data("lng"),
-                        content: "<div class='overlay'>"+$(this).data("description")+"</div>",
-                        verticalAlign: 'bottom'
-                });
-        }).on('focusout',()=>{
-                map.removeMarkers();
-                map.removeOverlays();
-        });         
+        ButtonHandler.listBtn(jsonObj);       
 });       
